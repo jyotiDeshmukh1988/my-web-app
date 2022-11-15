94ebb55a49ceb78e5219ae5420d7e2504c7faa37
jyotiDeshmukh1988 <83502437+jyotiDeshmukh1988@users.noreply.github.com>
2022-11-14 16:41:31 -0800

    accordian done

--- src/Reusable/Accordian/Accordian.js
+++ src/Reusable/Accordian/Accordian.js
@@ -2,15 +2,21 @@ import React from "react"
 import AccordianItem from "../AccordianItem"
 const Accordian = (props) =>{
     const {data} = props
-    return <div className="container accordian">
-    <div className="row">
-        {
-            data.map((obj,ind)=>{
-                return <AccordianItem item={obj} val={ind}/>
-            })
-        }
+    const [itemNo, setitemNo] = React.useState(0);
+    const fnClick = (eve) => {
+        //alert(eve.target.id)
+        setitemNo(eve.target.id == itemNo ? -1 : eve.target.id)
+       //alert(typeof(itemNo))
+    };
+        return <div className="container accordian">
+        <div className="row mt-5">
+            {
+                data.map((obj,ind)=>{
+                    return <AccordianItem item={obj} val={ind} fnClick={fnClick} isShow={itemNo == ind}/>
+                })
+            }
+            </div>    
         </div>
-    </div>
 }
 
 export default Accordian
--- src/Reusable/AccordianItem/AccordianItem.js
+++ src/Reusable/AccordianItem/AccordianItem.js
@@ -1,16 +1,11 @@
 import React from "react"
 import "./AccordianItem.css"
 const AccordianItem = (props) => {
-    const {item,val} = props
-    const [itemNo, setitemNo] = React.useState(0);
-    const fnClick = (eve) => {
-        //alert(eve.target.id)
-        setitemNo(eve.target.id)
-       //alert(typeof(itemNo))
-    };
+    const {item,val,fnClick,isShow} = props
     return <React.Fragment>
         <h3 onClick={fnClick} id={val}>{item.header}</h3>
-        <p className={val == itemNo ? 'show' :'hide'}>{item.body}</p>
+        {isShow && <p>{item.body}</p>}
+
     </React.Fragment>
   }  
 
