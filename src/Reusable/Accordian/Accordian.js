fa2f3bffcfb603af75d6b6e7da2da3d074a2bd6c
jyotiDeshmukh1988 <83502437+jyotiDeshmukh1988@users.noreply.github.com>
2022-11-17 18:54:18 -0800

    fixed the accordian issue

--- src/Reusable/Accordian/Accordian.js
+++ src/Reusable/Accordian/Accordian.js
@@ -4,15 +4,14 @@ const Accordian = (props) =>{
     const {data} = props
     const [itemNo, setitemNo] = React.useState(0);
     const fnClick = (eve) => {
-        //alert(eve.target.id)
+        alert(eve.target.id)
         setitemNo(eve.target.id === itemNo ? -1 : eve.target.id)
-       //alert(typeof(itemNo))
     };
         return <div className="container accordian">
         <div className="row mt-5">
             {
                 data.map((obj,ind)=>{
-                    return <AccordianItem key={ind} item={obj} val={ind} fnClick={fnClick} isShow={itemNo === ind}/>
+                    return <AccordianItem key={ind} item={obj} val={ind} fnClick={fnClick} isShow={itemNo == ind}/>
                 })
             }
             </div>    
--- src/Reusable/AccordianItem/AccordianItem.js
+++ src/Reusable/AccordianItem/AccordianItem.js
@@ -2,7 +2,9 @@ import React from "react"
 import "./AccordianItem.css"
 const AccordianItem = (props) => {
     const {item,val,fnClick,isShow} = props
+    console.log(props)
     return <React.Fragment>
+   
         <h3 onClick={fnClick} id={val}>{item.header}</h3>
         {isShow && <p>{item.body}</p>}
 
